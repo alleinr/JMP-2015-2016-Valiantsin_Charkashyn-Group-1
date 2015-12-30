a9cdc1252be77affe3350d3d2c1a8709a6c38db4
alleinr <alleinr@gmail.com>
2015-12-30 04:31:57 +0200

    initial module05 commit

---
+++ module05/task01/src/com/epam/jmp/classloading01/runner/Runner.java
@@ -0,0 +1,10 @@
+package com.epam.jmp.classloading01.runner;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ module05/task02/src/com/epam/jmp/classloading02/runner/Runner.java
@@ -0,0 +1,10 @@
+package com.epam.jmp.classloading02.runner;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
