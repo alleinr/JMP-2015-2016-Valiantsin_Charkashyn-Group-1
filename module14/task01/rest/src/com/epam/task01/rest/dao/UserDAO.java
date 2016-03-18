9f1494b66f77e718aaeca0053d6f8f59e044621e
alleinr <alleinr@gmail.com>
2016-03-18 19:01:15 +0300

    temp commit

---
+++ module14/task01/rest/src/com/epam/task01/rest/dao/UserDAO.java
@@ -0,0 +1,39 @@
+package com.epam.task01.rest.dao;
+
+import java.util.List;
+
+import com.epam.task01.rest.model.User;
+
+public class UserDAO implements IUserDAO{
+
+	@Override
+	public User createUser(User User) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User getUser(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User updateUser(User User) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean removeUser(String id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<User> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ module14/task01/restful/src/main/java/com/epam/task01/rest/dao/UserDAO.java
@@ -0,0 +1,35 @@
+package com.epam.task01.rest.dao;
+
+import java.util.List;
+
+import com.epam.task01.rest.model.User;
+
+public class UserDAO implements IUserDAO{
+
+	public User createUser(User User) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public User getUser(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public User updateUser(User User) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean removeUser(String id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public List<User> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
